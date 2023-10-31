ef3a4e680f10abd28721b085bf12a91af5706c44
Pei-Chen Tsai <pctsai13@gmail.com>
2023-10-31 15:43:02 +0000

    orders

--- models/models/intermediate/int_orders_margin.sql
+++ models/models/intermediate/int_orders_margin.sql
@@ -4,7 +4,6 @@ date_date,
 revenue,
 quantity,
 Purchase_cost,
-margin,
-ROUND (margin + shipping_fee - logcost - CAST(ship_cost AS FLOAT64),2) AS Operational_margin
+margin
 FROM {{ref("int_sales_margin")}} AS m
 JOIN {{ref("stg_raw__ship")}} AS s USING (orders_id)
