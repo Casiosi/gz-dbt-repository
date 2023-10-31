27a2bbbb26a238bc3eff3949865f92608ab90f7a
Pei-Chen Tsai <pctsai13@gmail.com>
2023-10-31 15:55:01 +0000

    aggregated

--- models/models/intermediate/int_orders_margin.sql
+++ models/models/intermediate/int_orders_margin.sql
@@ -1,9 +1,10 @@
 SELECT
 orders_id,
 date_date,
-revenue,
-quantity,
-Purchase_cost,
-margin
+ROUND(SUM(revenue),2) AS revenue,
+ROUND(sum(quantity),2) AS quantity,
+ROUND(SUM(Purchase_cost),2) AS Purchase_cost,
+ROUND(SUM(margin),2) AS margin
 FROM {{ref("int_sales_margin")}} AS m
 JOIN {{ref("stg_raw__ship")}} AS s USING (orders_id)
+GROUP BY orders_id, date_date
