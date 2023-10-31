ed2000c6825feab5dc529c199d3372ad17d1b5e7
Pei-Chen Tsai <pctsai13@gmail.com>
2023-10-31 16:29:37 +0000

    finance

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,9 @@
+SELECT
+date_date,
+COUNT(orders_id) AS nb_transcations,
+ROUND (SUM(revenue),2) AS revenue,
+ROUND(AVG (revenue),2) AS average_basket,
+ROUND(SUM (margin),2) AS margin,
+ROUND(SUM (Operational_margin),2) AS Operational_margin
+FROM {{ref("int_orders_operational")}}
+GROUP BY date_date
