9233e3f7fd55f7add97becd10e68af6e9edeeae0
Pei-Chen Tsai <pctsai13@gmail.com>
2023-11-01 10:15:02 +0000

    change join reference name on products_id=pdt_id

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,4 +3,4 @@ SELECT
 CAST(quantity AS INT64) * CAST(purchase_price AS FLOAT64) AS Purchase_cost,
 ROUND (CAST(revenue AS FLOAT64) - CAST(quantity AS INT64) * CAST(purchase_price AS FLOAT64),2) AS Margin
 FROM {{ref("stg_raw__sales")}} AS sales
-JOIN {{ref("stg_raw__product")}} AS product USING (products_id)
+JOIN {{ref("stg_raw__product")}} AS product ON sales.pdt_id = product.products_id
